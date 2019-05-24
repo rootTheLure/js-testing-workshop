e985e7e2b47512c4af3afb20f4ab16971c0f6736
Nikolay B <exedystyle@tutamail.com>
2019-05-24 20:57:51 +0300

    add some tests

--- specs/list.spec.js
+++ specs/list.spec.js
@@ -27,7 +27,7 @@ describe('linked list', function(){
     });
 
     describe('removeAt, removes item by index', function(){
-        it('decremens list size', function(){
+        it('should decrement list size', function(){
             let myList = new List();
             myList.add('value_1');
             myList.add('value_2');
@@ -37,11 +37,25 @@ describe('linked list', function(){
             let result = myList.size - 1; 
             
             myList.removeAt(2);
+            
             expect(myList.size).to.be.equal(result);
+        });
+
+        it('siblings of removed item should be connected', function(){
+            let myList = new List();
+            myList.add('value_1');
+            myList.add('value_2');
+            myList.add('value_3');
+            myList.add('value_4');
+            
+            let nextItem = myList.getAt(2);
+            myList.removeAt(1);
+            
+            expect(myList.head.next == nextItem).to.be.equal(true);
         });
     });
 
-    describe('index of value', function(){
+    describe('indexOf', function(){
         it('should return index of value', function(){
             let myList = new List();
             myList.add('value_1');
@@ -50,6 +64,7 @@ describe('linked list', function(){
             myList.add('value_4');
 
             let result = myList.indexOf('value_3');
+            
             expect(result).to.be.equal(2);
         });
     });
