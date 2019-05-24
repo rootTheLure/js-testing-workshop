eb61bdbbb987b2c3148ff02b82895686420b9a63
soulos410 <solimr111@mail.ru>
2019-05-24 20:52:35 +0300

    Add tests for Stack and Queue classes

---
+++ src/Queue.js
@@ -0,0 +1,21 @@
+class Queue {
+    constructor(size) {
+        this.maxSize = size;
+        this.currentSize = 0;
+        this.currentQueue = [];
+    }
+    push(...args) {
+
+    }
+    pop(n) {
+
+    }
+    top() {
+
+    }
+    merge(queueB, first) {
+
+    }
+}
+
+module.exports = Queue;
---
+++ src/Stack.js
@@ -0,0 +1,21 @@
+class Stack {
+    constructor(size) {
+        this.maxSize = size;
+        this.currentSize = 0;
+        this.stackArr = [];
+    }
+    push(...args) {
+
+    }
+    pop(count) {
+
+    }
+    top() {
+
+    }
+    merge(firstStack, secondStack) {
+
+    }
+}
+
+module.exports = Stack;
