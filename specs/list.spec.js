be1e678159756c7fdb93ff67f4d68a07279bd608
igor-provotorov <garynya@yandex.ru>
2019-05-24 21:00:19 +0300

    feat: add some test

--- specs/list.spec.js
+++ specs/list.spec.js
@@ -1,65 +1,148 @@
 var List = require('../src/List');
 
 describe('List', function () {
-    describe('sum', function () {
-        it ('should return the sum of two numbers', function() {
-            const list = new List();
-            let result = list.sum(2,3);
-            expect(result).to.be.equal(5);
-        });
-    });
-
     describe('add', function () {
         it ('should stores specified value to the list', function() {
             const list = new List();
-            let result = list.sum(2,3);
+            list.add('value1');
+            list.add('value2');
+            list.add('value3');
+            let result = list.indexOf('value2');
+            expect(result).to.be.equal(1);
+        });
+
+        it ('should stores specified value to the list', function() {
+            const list = new List();
+            list.add('value1');
+            list.add('value2');
+            list.add('value3');
+            list.add('value4');
+            let result = list.indexOf('value3');
+            expect(result).to.be.equal(2);
+        });
+        
+        it ('should stores specified value to the list', function() {
+            const list = new List();
+            list.add('value1');
+            list.add('value2');
+            list.add('value3');
+            list.add('value4');
+            let result = list.indexOf('value1');
+            expect(result).to.be.equal(0);
+        });
+        
+        it ('should returs valid length of list', function() {
+            const list = new List();
+            list.add('value1');
+            list.add('value2');
+            list.add('value3');
+            let result = list.length;
+            expect(result).to.be.equal(3);
+        });
+        
+        it ('should returs valid length of list', function() {
+            const list = new List();
+            list.add('value1');
+            list.add('value2');
+            list.add('value3');
+            list.add('value4');
+            list.add('value5');
+            let result = list.length;
             expect(result).to.be.equal(5);
+        });
+        
+        it ('should returs valid length of list', function() {
+            const list = new List();
+            list.add('value1');
+            list.add('value2');
+            list.add('value3');
+            list.add('value4');
+            list.add('value5');
+            list.add('value6');
+            list.add('value7');
+            list.add('value8');
+            list.add('value9');
+            list.add('value10');
+            let result = list.length;
+            expect(result).to.be.equal(10);
         });
     });
 
     describe('getAt', function () {
         it ('should returns element by specified index', function() {
             const list = new List();
-            let result = list.sum(2,3);
-            expect(result).to.be.equal(5);
+            list.add('value1');
+            list.add('value2');
+            list.add('value3');
+            let result = list.getAt(0);
+            expect(result).to.be.equal('value1');
+        });
+
+        it ('should returns element by specified index', function() {
+            const list = new List();
+            list.add('value1');
+            list.add('value2');
+            list.add('value3');
+            list.add('value4');
+            list.add('value5');
+            let result = list.getAt(4);
+            expect(result).to.be.equal('value5');
+        });
+        it ('should returns element by specified index', function() {
+            const list = new List();
+            list.add('value1');
+            list.add('value2');
+            list.add('value3');
+            list.add('value4');
+            list.add('value5');
+            list.add('value6');
+            list.add('value7');
+            list.add('value8');
+            list.add('value9');
+            list.add('value10');
+            let result = list.getAt(9);
+            expect(result).to.be.equal('value10');
         });
     });
 
     describe('removeAt', function () {
         it ('should removes item stored under specified index', function() {
+            const list = new List();        
+            list.add('value1');
+            list.add('value2');
+            list.add('value3');
+            list.removeAt(0);
+            let result = list.indexOf('value2');
+            expect(result).to.be.equal(0);
+        });
+        
+        it ('should returns valid length of list', function() {
             const list = new List();
-            let result = list.sum(2,3);
-            expect(result).to.be.equal(5);
+            list.add('value1');
+            list.add('value2');
+            list.add('value3');
+            list.removeAt(0);
+            let result = list.length;
+            expect(result).to.be.equal(2);
         });
     });
 
     describe('indexOf', function () {
         it ('should returns index of specified value', function() {
             const list = new List();
-            let result = list.sum(2,3);
-            expect(result).to.be.equal(5);
+            list.add('value1');
+            list.add('value2');
+            list.add('value3');            
+            let result = list.indexOf('value2');
+            expect(result).to.be.equal(1);
+        });
+        
+        it ('should not change length of list', function() {
+            const list = new List();
+            list.add('value1');
+            list.indexOf('value1');
+            let result = list.length;
+            expect(result).to.be.equal(1);
         });
     });
 });
-
-
-
-
-// const list = new List();
-
-// /**
-//   WARNING:
-//   Please use the suggested functions names!
-// **/
-
-// // stores specified value to the list;
-// list.add(value);
-
-// // returns element by specified index;
-// list.getAt(index);
-
-// // removes item stored under specified index;
-// list.removeAt(index);
-
-// // returns index of specified value;
-// list.indexOf(value);
--- src/List.js
+++ src/List.js
@@ -1,7 +1,27 @@
-function List () {}
+class List {
+    constructor () {
+        this.list = [];
+        this.length = 0;
+    }
 
-List.prototype.sum = function(a, b) {
-    return a + b;
-};
+    add(value) {
+        this.list.push(value);
+        this.length++;
+    }
+
+    getAt(index) {
+        return this.list[index];
+    }
+
+    removeAt(index) {
+        this.list.splice(index, 1);
+        this.length--;
+    }
+
+    indexOf(value) {
+        return this.list.indexOf(value);
+    }
+
+}
 
 module.exports = List;
