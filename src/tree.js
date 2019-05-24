7ab65ceba88346775901d0a0bdb19d1888537657
Ellina <ellinasereda18@gmail.com>
2019-05-24 20:53:03 +0300

    Add class which creates Tree

---
+++ src/tree.js
@@ -0,0 +1,21 @@
+/* function Tree() {}
+
+Tree.prototype.insert = function (key, value) {
+
+};
+module.exports = Tree;
+*/
+class Tree {
+    constructor() {
+
+    }
+
+    insert() {}
+
+    delete() {}
+
+    search() {}
+
+    contains() {}
+}
+module.exports = Tree;
