e49f3fec01c6f101a7b7e459b82decda21fee41f
soulos410 <solimr111@mail.ru>
2019-05-24 21:00:15 +0300

    Add Deque basic structure

---
+++ src/Deque.js
@@ -0,0 +1,19 @@
+class Deque {
+    constructor() {
+
+    }
+    push(left, ...args) {
+
+    }
+    pop(n, left) {
+
+    }
+    top(left) {
+
+    }
+    merge(dequeB, left) {
+
+    }
+}
+
+module.exports = Deque;
