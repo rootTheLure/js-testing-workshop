0ecbeaf905badba4cad1cb6e72891c8002c5bab5
Roman Chernyavski <romacher42@gmail.com>
2019-05-24 20:56:20 +0300

    Add tests for queue.

---
+++ src/queue.js
@@ -0,0 +1,16 @@
+class Queue {
+    constructor(n) {
+      this.maxSize = n;
+      this.queue = [];
+    }
+
+    push(...args) { }
+
+    pop(n) { }
+
+    top() { }
+
+    merge(queueB, first) { }
+}
+
+module.exports = Queue
